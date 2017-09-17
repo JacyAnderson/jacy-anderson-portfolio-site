b0ba749612ccfc6d5ee471b06993b2e2c2f62b2f
Jacy <jacyjamesanderson@gmail.com>
2017-09-17 15:19:15 -0600

    Update filestructure, add imagemin and css minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,29 @@
-var gulp = require('gulp');
-var concatCss = require('gulp-concat-css');
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
 
-gulp.task('default', function() {
-	return gulp.src('assets/**/*.css')
-	.pipe(concatCss("bundle.css"))
-	.pipe(gulp.dest('assets/css'));
+gulp.task('minify-css',() => {
+  return gulp.src('./assets/src/css/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('assets/dist/css'));
 });
+
+gulp.task('imagemin', () => 
+	gulp.src('./assets/src/images/*')
+		.pipe(imagemin([
+    	imagemin.gifsicle({interlaced: true}),
+    	imagemin.jpegtran({progressive: true}),
+    	imagemin.optipng({optimizationLevel: 5}),
+    	imagemin.svgo({plugins: [{removeViewBox: true}]})
+		]))
+		.pipe(gulp.dest('./assets/dist/images'))
+);
+
+gulp.task('watch', function() {
+	gulp.watch('./assets/src/css/*.css', ['minify-css']);
+});
+
+gulp.task('default', ['minify-css', 'imagemin', 'watch']);
